f145fc396c3fa34e730da7c4ca734b126447b094
nadasamyy <119449041+nadasamyy@users.noreply.github.com>
2024-12-28 02:25:52 +0000

    Delete src/org/example directory

--- src/org/example/UtilityClass.groovy
+++
@@ -1,8 +0,0 @@
-// src/org/example/UtilityClass.groovy
-package org.example
-
-class UtilityClass {
-    static String printMessage(String message) {
-        return "Message: ${message}"  // Return the message, do not echo
-    }
-}
